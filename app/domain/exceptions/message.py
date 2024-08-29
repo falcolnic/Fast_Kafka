be436d35f15798ad8f77407da1e5790fbaa6e093
falcolnic <mykh.yano@gmail.com>
2024-08-30 01:53:09 +0300

    added text and entities refactoring

--- app/domain/exceptions/message.py
+++ app/domain/exceptions/message.py
@@ -4,10 +4,15 @@ from domain.exceptions.base import ApplicationException
 
 
 @dataclass(eq=False)
-class TextTooLongException(ApplicationException):
+class TitleTooLongException(ApplicationException):
     text: str
 
     @property
     def message(self):
         return f'Text "{self.text}" is too long'
     
+@dataclass(eq=False)
+class EmptyTextException(ApplicationException):
+    @property
+    def message(self):
+        return 'Text cant be empty'
--- app/domain/values/messages.py
+++ app/domain/values/messages.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass
+
 from domain.values.base import BaseValueObject
-from domain.exceptions.messages import TextTooLongException
+from domain.exceptions.message import EmptyTextException, TitleTooLongException
+
 
 @dataclass(frozen=True)
 class Text(BaseValueObject):
     value: str
-    
+
     def validate(self):
+        if not self.value:
+            raise EmptyTextException()
+
+    def as_generic_type(self) -> str:
+        return str(self.value)
+
+
+@dataclass(frozen=True)
+class Title(BaseValueObject):
+    def validate(self):
+        if not self.value:
+            raise EmptyTextException()
+
         if len(self.value) > 255:
-            raise TextTooLongException(self.value)
-        
+            raise TitleTooLongException(self.value)
+
     def as_generic_type(self):
-        return self.value
+        return str(self.value)
