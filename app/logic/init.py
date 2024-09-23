5e0d6247835bf3c53baeeb7ad74ac11506b3e667
falcolnic <mykh.yano@gmail.com>
2024-09-23 14:45:34 +0300

    implemented an Query and Chat scheme

--- app/logic/init.py
+++ app/logic/init.py
@@ -7,6 +7,7 @@ from infra.repositories.messages.base import BaseChatsRepository, BaseMessagesRe
 from infra.repositories.messages.mongo import MongoDBChatsRepository, MongoDBMessagesRepository
 from logic.commands.messages import CreateChatCommand, CreateChatCommandHandler, CreateMessageCommand, CreateMessageCommandHandler
 from logic.mediator import Mediator
+from logic.queries.messages import GetChatDetailQuery, GetChatDetailQueryHandler
 from settings.config import Config
 
 
@@ -51,7 +52,7 @@ def _init_container() -> Container:
     # Commands and handlers
     container.register(CreateChatCommandHandler)
     container.register(CreateMessageCommandHandler)
-
+    container.register(GetChatDetailQueryHandler)
 
     # Mediator
     def init_mediator() -> Mediator:
@@ -64,6 +65,10 @@ def _init_container() -> Container:
         mediator.register_command(
             CreateMessageCommand,
             [container.resolve(CreateMessageCommandHandler)],
+        )
+        mediator.register_query(
+            GetChatDetailQuery,
+            container.resolve(GetChatDetailQueryHandler),
         )
 
         return mediator
---
+++ app/logic/queries/base.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Coroutine, Generic, TypeVar
+
+@dataclass(frozen=True)
+class BaseQuery(ABC):
+    ...
+
+QT = TypeVar('QT', bound=BaseQuery)
+QR = TypeVar('QR', bound=Any)
+
+@dataclass(frozen=True)
+class BaseQueryHandler(ABC, Generic[QT, QR]):
+    @abstractmethod
+    async def handle(self, query: QT) -> QR:
+        ... 
